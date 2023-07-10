b797968a8852754fdabc233cf155bf14f8018bfe
Arie Twigt <twigt.arie@gmail.com>
2023-07-10 14:24:19 +0200

    added examples

---
+++ assignments_3.py
@@ -0,0 +1,24 @@
+#%% Assignment 1. Create a dictionary with an (imaginary) person with attributes like his name, age and hobbies
+person_dict = {}
+person_dict['name'] = "Jim"
+person_dict['age'] = 30
+person_dict['hobbies'] = ['cycling', 'reading', 'football']
+
+# %%
+person_dict
+
+# %% Assignment 2
+person_2 = {"name": "mary", "age": 40}
+person_3 = {"name": "James", 
+            "age": 20, 
+            "hobbies": ['gaming', 'football']}
+
+family_dict = {}
+family_dict['name'] = "Jones"
+family_dict['members'] = [person_dict, person_2, person_3]
+
+
+# %%
+family_dict
+
+# %%
