db33875bfdc9aa4a5d58968ea40bab9284c54279
Arie Twigt <ariekanarie@twigtd4.nl>
2023-07-11 11:19:54 +0200

    added assignments 5

--- 5_assignments.py
+++ 5_assignments.py
@@ -1,28 +1,29 @@
 # Assignment 1 - Create a conditional statement that indicates if your name starts with an "A or not
 
 #%% a
-name = "Arie"
+name = "Dirk"
 
-if "a" in name.lower():
-    print("Your name begins with an A")
-
-#%% b
 if name[0].lower() == "a":
     print("Your name begins with an A")
+else:
+    print("Your name does not begin with an A")
+
 
 
 # Assignment 2 - Create a conditional statement that indicates if your name begins with a vowel. If it does, change it into a non-vowel and otherwise. For example: Arie –> Brie or Rose –> Aose
 
 #%%
-name = "Arie"
+name = "Rose"
 
-vowels = "aeoui"
+vowels_list = ['a', 'e', 'o', 'u', 'i']
 
-if name[0].lower() in vowels:
-    name = name.replace(name[0], "B")
-    print(name)
+if name[0].lower() in vowels_list:
+    new_name = name.replace(name[0], "B")
+    print(f"Old name: {name}, new name: {new_name}")
 else:
-    name = name.replace(name[0], "A")
+    new_name = name.replace(name[0], "A")
+    print(f"Old name: {name}, new name: {new_name}")
+
 
 
 #%% b
--- import_rdw.py
+++ import_rdw.py
@@ -1,5 +1,6 @@
 #%%
 import requests
+import sys
 
 selected_brand = input("Voer merk in:\n")
 selected_brand_upper = selected_brand.upper()
@@ -8,9 +9,13 @@ endpoint = f"https://opendata.rdw.nl/resource/m9d7-ebf2.json?merk={selected_bran
 
 response = requests.get(endpoint)
 
-
 # %%
 data = response.json()
+
+#%%
+if len(data) == 0:
+    print(f"No cars found for {selected_brand}")
+    sys.exit()
 
 #%%
 print(data[0])
