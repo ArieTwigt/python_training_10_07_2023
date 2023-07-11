63549e435091ecc1190bb3ba81dd4756ff4d4a35
Arie Twigt <ariekanarie@twigtd4.nl>
2023-07-11 13:07:53 +0200

    added assignment 4

--- 6_assignments.py
+++ 6_assignments.py
@@ -1,27 +1,17 @@
 #%% Assignment 1
-
-vowels_list = ['a', 'e', 'o', 'u', 'i']
-
 names_list = ['Jim', 'John', 'Marc', 'Danny', 'Peter']
 
+vowels_list = ['a', 'e', 'o', 'u', 'i', 'y']
+
+new_names_list = []
+
 for name in names_list:
-    for letter in name:
-        if letter in vowels_list:
-            name = name.replace(letter, "")
-    
     print(name)
+    for letter in name:
+        if letter.lower() in vowels_list:
+            name = name.replace(letter, "")
+    new_names_list.append(name)
 
-# %% Assignment 2 - Create a loop that prints the name of the day for the following 10 days
-from datetime import date, timedelta
-
-current_date = date.today()
-
-num_days = 10
-
-for day_num in range(1, num_days + 1):
-    new_date = current_date + timedelta(days=day_num)
-    new_day_name = new_date.strftime("%A")
-    print(new_day_name)
 
 
 # %%
