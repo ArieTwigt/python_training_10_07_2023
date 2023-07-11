5f5bad911cc20b93cb0c95196269e509d49e59dc
Arie Twigt <ariekanarie@twigtd4.nl>
2023-07-11 11:58:01 +0200

    added assignments for 6

--- 6_assignments.py
+++ 6_assignments.py
@@ -10,4 +10,18 @@ for name in names_list:
             name = name.replace(letter, "")
     
     print(name)
+
+# %% Assignment 2 - Create a loop that prints the name of the day for the following 10 days
+from datetime import date, timedelta
+
+current_date = date.today()
+
+num_days = 10
+
+for day_num in range(1, num_days + 1):
+    new_date = current_date + timedelta(days=day_num)
+    new_day_name = new_date.strftime("%A")
+    print(new_day_name)
+
+
 # %%
